7fcd5e7fa7ca4580e57cf101947868539cd52bb9
VictorOgbebor <victor.ogbebor@outlook.com>
2022-06-28 03:00:29 -0500

    add some deploy scripts

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,11 +1,62 @@
 // deploy MATIC/ Kovan / BNB / CRONOS
-const { ethers } = require("ethers")
+const ethers = require("ethers")
+
+const ETHToken = artifacts.require("./ETHToken")
+const ETHBridge = artifacts.require("./ETHBridge")
+
+const BSCToken = artifacts.require("./BSCToken")
+const BSCBridge = artifacts.require("./BSCBridge")
 
 
-const ETHToken = require("./ETHToken")
-const ETHBridge = require("./ETHBridge")
+module.exports = async function (deployer, network, addresses) {
 
-const BSCToken = require("./BSCToken")
-const BSCBridge = require("./BSCBridge")
+    const name = "CrossChainTokenBic"
+    const symbol = "BIC"
+    const supply = ethers.utils.parseUnits('1000000', 'ether') // 1,000,000 Tokens
 
+    // Ideally we first deploy to rinkeby, then we deploy to binance testnet.
 
+    if (network === 'rinkeby') {
+        await deployer.deploy(ETHToken, name, symbol)
+        const token = await ETHToken.deployed()
+
+        await token.mint(addresses[0], supply)
+
+        await deployer.deploy(ETHBridge, token.address)
+        const bridge = await ETHBridge.deployed()
+
+        await token.setBridge(bridge.address)
+    }
+
+    if (network === 'bsc_testnet') {
+        await deployer.deploy(BSCToken, name, symbol)
+        const token = await BSCToken.deployed()
+
+        await deployer.deploy(BSCBridge, token.address)
+        const bridge = await BSCBridge.deployed()
+
+        await token.setBridge(bridge.address)
+    }
+
+    if (network === 'development') {
+        let token, bridge
+
+        await deployer.deploy(ETHToken, name, symbol)
+        token = await ETHToken.deployed()
+
+        await token.mint(addresses[0], supply)
+
+        await deployer.deploy(ETHBridge, token.address)
+        bridge = await ETHBridge.deployed()
+
+        await token.setBridge(bridge.address)
+
+        await deployer.deploy(BSCToken, name, symbol)
+        token = await BSCToken.deployed()
+
+        await deployer.deploy(BSCBridge, token.address)
+        bridge = await BSCBridge.deployed()
+
+        await token.setBridge(bridge.address)
+    }
+}
